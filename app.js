aa3e385aa554baa8964f55d3d62e3d657adda1a2
brisey006 <brisey2006@gmail.com>
2020-06-16 22:27:50 +0200

    Added morgan

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const passport = require('passport');
 const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -15,6 +16,7 @@ mongoose.set('useUnifiedTopology', true);
 const app = express();
 
 require('dotenv').config();
+app.use(morgan('tiny'));
 //Passport configuration
 require('./config/passport')(passport);
 
